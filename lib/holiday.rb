5e933c64a0be6cb4f706cbea119861f948107455
John Stassen <john.stassen@gmail.com>
2020-08-23 04:03:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,6 +43,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
     holiday_hash[season_bucket].merge({holiday_name: supply_array})
     binding.pry
   end
+  end
   # remember to return the updated hash
   holiday_hash
 end
