03df21c8e787e556210c3dc086184f1f0bcadfee
John Stassen <john.stassen@gmail.com>
2020-08-23 03:40:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,12 +38,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
-  holiday_hash.keys.each do |key|
-    if key == season
-      holiday_hash.keys << holiday_name
-    end
-  end
-  holiday_hash
+  holiday_hash[season] << {holiday_name: supply_array}
   # remember to return the updated hash
 
 end
