22fc3f75e46489dc7993b8561d970df961b00449
John Stassen <john.stassen@gmail.com>
2020-08-23 03:32:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,8 +38,11 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
+  holiday_hash.each do |season_bucket, seaon_holidays|
+    if season_bucket == season
+      holiday_hash[season_bucket] << {holiday name: supply_array}  
   # remember to return the updated hash
-
+  
 end
 
 def all_winter_holiday_supplies(holiday_hash)
