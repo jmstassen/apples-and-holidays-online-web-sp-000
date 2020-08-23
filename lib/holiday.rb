6f574282923bc85bf3e9df420fdbe7d5654e05ed
John Stassen <john.stassen@gmail.com>
2020-08-23 04:58:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_data|
     puts "#{season.capitalize}:"
     holiday_data.each do |holiday, supplies|
-      print_holiday = "#{holiday}"
+      print_holiday = titleize("#{holiday}")
       puts "  #{print_holiday.titleize}: #{supplies.join(", ")}"
     end
   end
